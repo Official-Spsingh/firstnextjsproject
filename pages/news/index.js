8e6a3fe9d69c33d240af389eb1ab870ca6f62433
Shubham Pratap Singh <SPSingh@bpl.netlink.com>
2022-06-12 16:31:41 +0530

    changes

--- pages/news/index.js
+++ pages/news/index.js
@@ -6,9 +6,9 @@ const NewsPage = () => {
         <ul>
             <li><Link href="/news/news1">News1 -- No refresh</Link></li>
             <li><Link href="/news/news2">News2 -- No refrsh</Link></li>
-            <li><a href="/news/news3">News3</a></li>
-            <li><a href="/news/news4">News4</a></li>
-            <li><Link href="/news/news5">Normal html image -- No refresh</Link></li>
+            {/* <li><a href="/news/news3">News3</a></li>
+            <li><a href="/news/news4">News4</a></li> */}
+            {/* <li><Link href="/news/news5">Normal html image -- No refresh</Link></li> */}
             <li><Link href="/news/news6">Next image -- No refresh</Link></li>
             <Link href="/">
                 <a>← Back to home</a>
